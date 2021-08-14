12929a560d0c2f62cac6a20ff5d61d72fa1731e5
Steven Lei <stevenplus@gmail.com>
2021-08-14 14:53:35 +0800

    add tailwindcss

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CodingStarter</title>
 
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     @livewireStyles
 </head>
 <body>
