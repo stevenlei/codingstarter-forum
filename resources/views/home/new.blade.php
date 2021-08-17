cce7da819d73c3ad127d25e092203074d29ac59e
Steven Lei <stevenplus@gmail.com>
2021-08-17 15:12:03 +0800

    add back link for /new for views bigger than mobile

--- resources/views/home/new.blade.php
+++ resources/views/home/new.blade.php
@@ -28,6 +28,12 @@
 </div>
 <div class="w-full sm:w-1/2 lg:w-3/4 bg-gray-800">
   <div class="overflow-auto h-screen-center-inner">
+    <a href="{{ url('/') }}" class="block hidden sm:block flex fill-current text-purple-600 opacity-60 text-xl mb-2 mt-4 ml-4">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8 inline-block">
+          <path fill="currentColor" d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42 -.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
+      </svg>
+      <span class="relative top-0.5">Back</span>
+    </a>
     <form action="{{ url('/new') }}" method="post">
       @csrf
       <input name="title" class="text-gray-200 font-bold text-2xl py-4 px-6 sticky top-0 bg-gray-800 outline-none w-full" placeholder="Title here..." value="{{ old('title') }}">
