662f8e10f3a255586311a253f306b776f65f87fd
zixu <57753561+Zixuip@users.noreply.github.com>
2021-08-18 22:50:11 +0800

    update css

--- resources/views/home/new.blade.php
+++ resources/views/home/new.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.master')
 
 @section('postheader')
-<a href="{{ url('/') }}" class="block sm:hidden flex fill-current text-purple-600 opacity-60 text-xl mb-2 mt-6">
+<a href="{{ url('/') }}" class="sm:hidden flex fill-current text-purple-600 opacity-60 text-xl mb-2 mt-6">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
-      <path fill="currentColor" d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42 -.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
+    <path fill="currentColor" d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42 -.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
   </svg>
   <span class="relative top-0.5">Back</span>
 </a>
@@ -16,7 +16,7 @@
       <span class="block px-6 py-4">/new</span>
     </li>
     @foreach ($topics as $list_topic)
-    <li class="bg-gray-200 text-gray-500 border-gray-200 dark:text-purple-400 dark:bg-gray-900 group border-b border-gray-300 dark:border-gray-800 opacity-40">
+    <li class="bg-gray-200 text-gray-500 dark:text-purple-400 dark:bg-gray-900 group border-b border-gray-300 dark:border-gray-800 opacity-40">
       <a href="{{ url('/post/' . $list_topic->id) }}" class="py-4 px-6 block pointer-events-none">
         <h4 class="font-bold truncate">{{ $list_topic->title }}</h4>
         <p class="text-gray-600 leading-tight break-all">{{ $list_topic->posts->last()->excerpt() }}</p>
@@ -28,9 +28,9 @@
 </div>
 <div class="w-full sm:w-1/2 lg:w-3/4 bg-gray-100 dark:bg-gray-800">
   <div class="overflow-auto h-screen-center-inner">
-    <a href="{{ url('/') }}" class="block hidden sm:block flex fill-current text-purple-600 opacity-60 text-xl mb-2 mt-4 ml-4">
+    <a href="{{ url('/') }}" class="sm:block flex fill-current text-purple-600 opacity-60 text-xl mb-2 mt-4 ml-4">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8 inline-block">
-          <path fill="currentColor" d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42 -.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
+        <path fill="currentColor" d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42 -.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
       </svg>
       <span class="relative top-0.5">Back</span>
     </a>
