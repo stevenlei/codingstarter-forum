392583b6f70f70bec5c56b586b63aa29213fb6c6
Steven Lei <stevenplus@gmail.com>
2021-08-18 10:44:45 +0800

    loading icon should be position absolute to prevent moving the existing layouts

--- resources/views/livewire/show-topic.blade.php
+++ resources/views/livewire/show-topic.blade.php
@@ -1,5 +1,5 @@
-<div class="w-full sm:w-1/2 lg:w-3/4 bg-gray-800 main-content">
-	<div class="mt-5 ml-6" wire:loading>
+<div class="w-full sm:w-1/2 lg:w-3/4 bg-gray-800 main-content relative">
+	<div class="absolute top-5 left-6" wire:loading>
         <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
